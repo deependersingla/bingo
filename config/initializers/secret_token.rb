4e9c52dd8bef65934527770d75a7c083ff2529a0
Deepender <deepender281190@gmail.com>
2014-03-02 13:58:32 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bingo::Application.config.secret_key_base = '6a21577eb06e8672201c02693d61ade5444073d038fdb124926ad0418ce4f882fd16aff95ddef5f81f79b6bc00e44d8290462e264b67b5f78664f4e5334b808b'
