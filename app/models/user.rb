ab7bf4ec7b16c43a5a0945b73b40824f489dcd96
Deepender <deepender281190@gmail.com>
2014-03-04 12:21:35 +0530

    models and view created

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,8 @@ class User
   field :last_sign_in_at,    :type => Time
   field :current_sign_in_ip, :type => String
   field :last_sign_in_ip,    :type => String
+  has_many :game
+
 
   ## Confirmable
   # field :confirmation_token,   :type => String
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Bingo::Application.routes.draw do
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
+  match ':controller(/:action(/:id))(.:format)', via: [:get, :post]
 end
