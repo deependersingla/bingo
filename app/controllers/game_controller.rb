a08330bd8181f5347b15064483c3a76dc795f995
Rachana Ramchand <rachana.ramchand@gmail.com>
2014-05-03 15:01:31 +0530

    tie

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -11,19 +11,27 @@ class GameController < ApplicationController
     @matrix = game.opponent_matrix
   end
 
+  def computer_win
+    game = Game.where(user_id: current_user.id).last
+    @matrix = game.opponent_matrix
+  end
+
   def play
     game = Game.where(user_id: current_user.id).last
     # computer
     comp = ComputerPlay.new(game.opponent_matrix)
     cut_lines = comp.total_cut_lines
-    if cut_lines < 5
-    	@matrix = game.starter_matrix
-    else
-      redirect_to :action => :computer_win
-    end
-  	# add a partial here so that ajax can be used
+    if cut_lines <= 7
+    @matrix = game.starter_matrix
+    
+    #else 
+    #  redirect_to :action => :computer_win
+    # add a partial here so that ajax can be used
   end
+end
    skip_before_action :verify_authenticity_token
+
+
   def search
     Game.new.update_matrix(params[:q],current_user)
     redirect_to :action => :play
