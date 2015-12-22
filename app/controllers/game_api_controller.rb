90eb2356ee1ad8ef2e961304a81100a045883744
Ryan Booth <ryanbooth77@gmail.com>
2015-12-22 01:13:32 -0800

    reformatting for readability

--- app/controllers/game_api_controller.rb
+++ app/controllers/game_api_controller.rb
@@ -9,6 +9,7 @@ class GameApiController < ApplicationController
       game = Game.new.game_initialization(ip, 5) unless game
       comp = ComputerPlay.new(game.opponent_matrix)
       @comp_cut_lines = comp.total_cut_lines
+
       if @comp_cut_lines >= game.level 
         last_level = ip.game.last.level
         ip.computer_win += 1
@@ -18,35 +19,37 @@ class GameApiController < ApplicationController
         params[:human_cut_lines] = 0
         @comp_cut_lines = comp.total_cut_lines
       end
-        @matrix = game.starter_matrix
-        @comp_matrix = game.opponent_matrix
-        hum = ComputerPlay.new(@matrix)
-        @human_cut_lines = hum.total_cut_lines
-        unless params[:human_cut_lines]
-          params[:human_cut_lines] = 0
-        end
-        if params[:wrong_number]
-          reward = 0
+
+      @matrix = game.starter_matrix
+      @comp_matrix = game.opponent_matrix
+      hum = ComputerPlay.new(@matrix)
+      @human_cut_lines = hum.total_cut_lines
+      unless params[:human_cut_lines]
+        params[:human_cut_lines] = 0
+      end
+      if params[:wrong_number]
+        reward = 0
+      else
+        if @human_cut_lines == 5
+          ip.human_win += 1
+          ip.save
+          reward = 3
+        elsif @human_cut_lines > params[:human_cut_lines].to_i
+          reward = 1
         else
-          if @human_cut_lines == 5
-            ip.human_win += 1
-            ip.save
-            reward = 3
-          elsif @human_cut_lines > params[:human_cut_lines].to_i
-            reward = 1
-          else
-            reward = 0
-          end 
-        end
-        render json: {
-          matrix: @matrix,
-          input_vector: mapping_of_number(@matrix),
-          human_cut_lines: @human_cut_lines ,
-          reward: reward,
-          human_win: ip.human_win,
-          computer_win: ip.computer_win,
-          comp_cut_lines: @comp_cut_lines
-        }
+          reward = 0
+        end 
+      end
+
+      render json: {
+        matrix: @matrix,
+        input_vector: mapping_of_number(@matrix),
+        human_cut_lines: @human_cut_lines ,
+        reward: reward,
+        human_win: ip.human_win,
+        computer_win: ip.computer_win,
+        comp_cut_lines: @comp_cut_lines
+      }
     else 
       render json: {
       	text: "please give id"
