ca5a1330dba89dd837f743dd9a9a81d159367acf
deepender <deepender281190@gmail.com>
2015-12-30 13:12:27 +0530

    controller updated

--- app/controllers/game_api_controller.rb
+++ app/controllers/game_api_controller.rb
@@ -28,7 +28,7 @@ class GameApiController < ApplicationController
         params[:human_cut_lines] = 0
       end
       if params[:wrong_number]
-        reward = 0
+        reward = -1
       else
         if @human_cut_lines == 5
           ip.human_win += 1
@@ -68,7 +68,59 @@ class GameApiController < ApplicationController
     else
       params[:wrong_number] = true
     end
-    redirect_to :action => :play_game, params: params and return
+    if Ip.where(ip: params[:user_id]).exists? 
+      ip = Ip.where(ip: params[:user_id]).last
+      game = ip.game[-1]
+      game = Game.new.game_initialization(ip, 5) unless game
+      comp = ComputerPlay.new(game.opponent_matrix)
+      @comp_cut_lines = comp.total_cut_lines
+
+      if @comp_cut_lines >= game.level 
+        last_level = ip.game.last.level
+        ip.computer_win += 1
+        ip.save
+        game = Game.new.game_initialization(ip, last_level)
+        comp = ComputerPlay.new(game.opponent_matrix)
+        params[:human_cut_lines] = 0
+        @comp_cut_lines = comp.total_cut_lines
+      end
+
+      @matrix = game.starter_matrix
+      @comp_matrix = game.opponent_matrix
+      hum = ComputerPlay.new(@matrix)
+      @human_cut_lines = hum.total_cut_lines
+      unless params[:human_cut_lines]
+        params[:human_cut_lines] = 0
+      end
+      if params[:wrong_number]
+        reward = -1
+      else
+        if @human_cut_lines == 5
+          ip.human_win += 1
+          ip.save
+          reward = 3
+        elsif @human_cut_lines > params[:human_cut_lines].to_i
+          reward = 1
+        else
+          reward = 0
+        end 
+      end
+
+      render json: {
+        matrix: @matrix,
+        input_vector: mapping_of_number(@matrix),
+        human_cut_lines: @human_cut_lines ,
+        reward: reward,
+        human_win: ip.human_win,
+        computer_win: ip.computer_win,
+        comp_cut_lines: @comp_cut_lines,
+        illegal_move: params[:wrong_number]
+      }
+    else 
+      render json: {
+        text: "please give id"
+      }
+    end
   end
 
   protected
